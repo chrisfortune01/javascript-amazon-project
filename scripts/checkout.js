eb22876ba08e01ab550d3815f539ffa3fc263dfd
Chris-Fortune01 <chrisfortune01@gmail.com>
2026-09-19 00:25:23 +0100

    Fix file indentation

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,18 +1,20 @@
 import { cart } from "../data/cart.js ";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 updateCheckoutQuantity()
 renderOrderSummary()
+renderPaymentSummary()
 
 function updateCheckoutQuantity() {
   let cartQuantity = 0;
-    cart.forEach(cartItem => {
-      cartQuantity += cartItem.quantity;
-    })
-    if (cartQuantity > 1) {
-        document.querySelector('.js-return-to-home-link')
-            .innerHTML = `${cartQuantity} Items`;
-    } else {
-        document.querySelector('.js-return-to-home-link')
-            .innerHTML = `${cartQuantity} Item`;
-    }
+  cart.forEach(cartItem => {
+    cartQuantity += cartItem.quantity;
+  })
+  if (cartQuantity > 1) {
+    document.querySelector('.js-return-to-home-link')
+      .innerHTML = `${cartQuantity} Items`;
+  } else {
+    document.querySelector('.js-return-to-home-link')
+      .innerHTML = `${cartQuantity} Item`;
+  }
 };
