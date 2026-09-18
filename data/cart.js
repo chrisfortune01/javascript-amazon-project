9ebab93d80a556746b70933818a201dfa3221980
Chris-Fortune01 <chrisfortune01@gmail.com>
2026-09-18 22:16:39 +0100

    Regenerate HTML when updating delivery option

--- data/cart.js
+++ data/cart.js
@@ -64,6 +64,5 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     }
   });
   matchingItem.deliveryOptionId = deliveryOptionId;
-  console.log(matchingItem)
   saveToStorage();
 }
