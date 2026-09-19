fd5fdf8f37276f624065e313ecb21dcb1fbb744d
Chris-Fortune01 <chrisfortune01@gmail.com>
2026-09-19 03:18:38 +0100

    Seperate the date calculation to a function

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,7 @@
 import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js ";
 import { getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { calculateDeliveryDate, deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { updateCheckoutQuantity } from "../checkout.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 
@@ -17,9 +16,7 @@ export function renderOrderSummary() {
     const deliveryOptionId = cartItem.deliveryOptionId;
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays,'days')
-    const dateString = deliveryDate.format('dddd, MMMM D')
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSumarrayHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -67,9 +64,7 @@ export function renderOrderSummary() {
     let html = ''
 
     deliveryOptions.forEach(deliveryOption => {
-      const today = dayjs();
-      const deliveryDate = today.add(deliveryOption.deliveryDays,'days')
-      const dateString = deliveryDate.format('dddd, MMMM D')
+      const dateString = calculateDeliveryDate(deliveryOption);
 
       const priceString = deliveryOption.priceCents === 0 
       ? 'FREE'  //returs this value if condition is true for priceString
