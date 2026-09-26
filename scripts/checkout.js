9a974e812de162bed451ac01416d3cabd451a81c
Chris-Fortune01 <chrisfortune01@gmail.com>
2026-09-26 04:00:17 +0100

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import { updateCartQuantity } from "../data/cart.js";
 import { cart } from "../data/cart.js ";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
+
 updateCheckoutQuantity()
 renderOrderSummary()
 renderPaymentSummary()
