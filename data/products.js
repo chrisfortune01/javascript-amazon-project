64ed48482c89a332476375d61f53fabf50fe2435
Chris-Fortune01 <chrisfortune01@gmail.com>
2026-09-26 17:13:12 +0100

    Converts loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -67,6 +67,29 @@ logThis.call('hello')
 
 export let products = [];
   
+
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    // console.log(response)
+    return response.json();
+  }).then(productsData => {
+    // console.log(productsData)
+    products = productsData.map(productDetails => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    // console.log(products)
+
+    return promise;
+  })
+}
+// loadProductsFetch()
+
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -87,6 +110,7 @@ export function loadProducts(fun) {
   xhr.send();
 }
 // loadProducts()
+
 
 /*
 export const products = [
